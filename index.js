841e980787d6a206302a9d87e0bea8e194bf7b4a
Gigi M <mmgamao1@gmail.com>
2019-08-11 20:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+/*
+// const oldID = Math.floor(Math.random() * 100) + 1;
+
+// const ages = [26.4, 21, 49.5, 83, 99, 53.3, 74, 94.9, 40, 56.2];
+
+// const currentAge = ages[Math.floor(Math.random() * ages.length)];
+
+// const currentAgeIsInteger = Number.isInteger(currentAge);
+
+// const spyOnNumberIsInteger = sinon.stub(Number, 'isInteger').returns(currentAgeIsInteger);
+
+// const randNum = Math.random();
+
+// const spyOnMathRandom = sinon.stub(Math, 'random').returns(randNum);
+*/
+
+const newID = 1000000000 + oldID;
+
+const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumber = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000001;
+
+const multiply = var1 * var2
